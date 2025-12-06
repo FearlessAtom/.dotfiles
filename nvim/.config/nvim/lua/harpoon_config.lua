0042a705ef23900769eca84d7ceaf870c757c64a
FearlessAtom <atomgotaggressive@gmail.com>
2025-12-06 22:08:12 +0200

    fix: change the bindings for harpoon

--- nvim/.config/nvim/lua/harpoon_config.lua
+++ nvim/.config/nvim/lua/harpoon_config.lua
@@ -3,15 +3,14 @@ local harpoon = require("harpoon")
 harpoon.setup()
 
 vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>b", function() harpoon:list():add() end)
 
-vim.keymap.set("n", "<Space-a>", "", {})
 
-vim.keymap.set({"n", "i"}, "<C-S-a>", function() harpoon:list():select(1) end)
-vim.keymap.set({"n", "i"}, "<C-S-s>", function() harpoon:list():select(2) end)
-vim.keymap.set({"n", "i"}, "<C-S-d>", function() harpoon:list():select(3) end)
-vim.keymap.set({"n", "i"}, "<C-S-f>", function() harpoon:list():select(4) end)
-vim.keymap.set({"n", "i"}, "<C-S-g>", function() harpoon:list():select(5) end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():select(1) end, { noremap = true })
+vim.keymap.set("n", "<leader>s", function() harpoon:list():select(2) end, { noremap = true })
+vim.keymap.set("n", "<leader>d", function() harpoon:list():select(3) end, { noremap = true })
+vim.keymap.set("n", "<leader>f", function() harpoon:list():select(4) end, { noremap = true })
+vim.keymap.set("n", "<leader>g", function() harpoon:list():select(5) end, { noremap = true })
 
 vim.keymap.set("n", "C-S-j", function() harpoon:list():prev() end)
 vim.keymap.set("n", "C-S-k", function() harpoon:list():next() end)
