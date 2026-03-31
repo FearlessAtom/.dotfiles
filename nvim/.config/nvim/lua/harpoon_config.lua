1237e64da8cee7812fe1882a8124cb296cc2954a
FearlessAtom <atomgotaggressive@gmail.com>
2026-03-31 19:21:49 +0300

    feat: change mapping for harpoon

--- nvim/.config/nvim/lua/harpoon_config.lua
+++ nvim/.config/nvim/lua/harpoon_config.lua
@@ -3,14 +3,13 @@ local harpoon = require("harpoon")
 harpoon.setup()
 
 vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<leader>b", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+ 
+vim.keymap.set("n", "!", function() harpoon:list():select(1) end, { noremap = true })
+vim.keymap.set("n", "@", function() harpoon:list():select(2) end, { noremap = true })
+vim.keymap.set("n", "#", function() harpoon:list():select(3) end, { noremap = true })
+vim.keymap.set("n", "$", function() harpoon:list():select(4) end, { noremap = true })
+vim.keymap.set("n", "%", function() harpoon:list():select(5) end, { noremap = true })
 
-
-vim.keymap.set("n", "<leader>a", function() harpoon:list():select(1) end, { noremap = true })
-vim.keymap.set("n", "<leader>s", function() harpoon:list():select(2) end, { noremap = true })
-vim.keymap.set("n", "<leader>d", function() harpoon:list():select(3) end, { noremap = true })
-vim.keymap.set("n", "<leader>f", function() harpoon:list():select(4) end, { noremap = true })
-vim.keymap.set("n", "<leader>g", function() harpoon:list():select(5) end, { noremap = true })
-
-vim.keymap.set("n", "C-S-j", function() harpoon:list():prev() end)
-vim.keymap.set("n", "C-S-k", function() harpoon:list():next() end)
+vim.keymap.set("n", "H", function() harpoon:list():prev() end)
+vim.keymap.set("n", "L", function() harpoon:list():next() end)
--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,9 +1,7 @@
 vim.keymap.set("n", "<Space>", "<Nop>", { noremap = true })
 
-vim.keymap.set("n", "#", "^", {})
-
-vim.keymap.set({"n", "v"}, "3", "^", {})
-vim.keymap.set({"n", "v"}, "4", "$", {})
+vim.keymap.set({"n", "v"}, "<C-h>", "^", { noremap = true, silent = true })
+vim.keymap.set({"n", "v"}, "<C-l>", "$", { noremap = true, silent = true })
 
 vim.keymap.set("i", "<C-BS>", "<C-w>", {});
 
@@ -14,9 +12,9 @@ vim.keymap.set({"n", "i"}, "<down>", "", {})
 vim.keymap.set({"n", "i"}, "<left>", "", {})
 vim.keymap.set({"n", "i"}, "<right>", "", {})
 
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
+--vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+--vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+--vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
+--vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 
 vim.keymap.set({"i", "n"}, "<C-s>", "<cmd>write<CR>")
